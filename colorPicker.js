44da53e8441ed270c6edd67daad2df027307bcf3
Emir Balic <emirbalic@gmail.com>
2018-09-21 20:52:15 +0200

    Add files via upload

---
+++ colorPicker.js
@@ -0,0 +1,17 @@
+var colors = [
+  "RGB(255, 0, 0)",
+  "RGB(255, 255, 0)",
+  "RGB(0, 255, 0)",
+  "RGB(0, 255, 255)",
+  "RGB(0, 0, 255)",
+  "RGB(255, 0, 255)"
+];
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById('colorDisplay');
+colorDisplay.textContent = pickedColor; 
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].style.backgroundColor = colors[i];
+}
