2ca837cbb9b7ef0f8c5e54d97d53cf6bfeb41f5d
Emir Balic <emirbalic@gmail.com>
2018-09-21 21:56:39 +0200

    colors match, btw - css default is small letters

--- colorPicker.js
+++ colorPicker.js
@@ -1,10 +1,10 @@
 var colors = [
-  "RGB(255, 0, 0)",
-  "RGB(255, 255, 0)",
-  "RGB(0, 255, 0)",
-  "RGB(0, 255, 255)",
-  "RGB(0, 0, 255)",
-  "RGB(255, 0, 255)"
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
 ];
 
 var squares = document.querySelectorAll(".square");
@@ -12,6 +12,23 @@ var pickedColor = colors[3];
 var colorDisplay = document.getElementById('colorDisplay');
 colorDisplay.textContent = pickedColor; 
 
+ 
+
 for (var i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i];
+
+  squares[i].addEventListener('click', function pickColor(){
+    
+    var clickedColor = this.style.backgroundColor;
+    
+    console.log(clickedColor, pickedColor);
+    if(clickedColor === pickedColor){
+      alert('opaaaa');
+    } else {
+      alert('nononono');
+    }
+    
+  });
 }
+
+
